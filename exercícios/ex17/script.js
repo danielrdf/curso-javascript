c625af7390e21cebcbacbbc0b017552572a7b47b
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-07-31 12:19:44 -0300

    ex17

--- exercícios/ex17/script.js
+++ exercícios/ex17/script.js
@@ -1,1 +1,32 @@
-alert('Olá!')
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var pas = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            // contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{25AA} `
+            }
+        } else {
+            // contagem decrescente
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{25AA} `
+            }
+        }
+        res.innerHTML += `\u{1F6A9}`
+    }
+}
