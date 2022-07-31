d32c3a04c33b4d49c9f7e513350aaa7d36d0dd75
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-07-31 20:28:09 -0300

    ex19

---
+++ exercícios/ex19/script.js
@@ -0,0 +1,20 @@
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
+var valores = []
+
+function isNumero(n) {
+
+}
+
+function inLista(n) {
+    
+}
+
+function adicionar () {
+    if (isNumero(num.value) && !inLista(num.value)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
