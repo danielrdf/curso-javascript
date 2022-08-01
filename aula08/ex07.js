83ab332056ef046ca8ac9e489ee3b9837d56e918
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-01 18:17:10 -0300

    Update ex07.js

--- aula08/ex07.js
+++ aula08/ex07.js
@@ -2,3 +2,11 @@ console.log(5 == 5)
 console.log(5 == '5')
 console.log(5 === '5') // são idênticos? têm mesmo valor e são de mesmo tipo?
 console.log(5 === 5)
+
+// operador ternário
+var media = 8
+console.log(media > 7 ? 'Aprovado' : 'Reprovado')
+
+var x = 8
+var res = x % 2 == 0 ? 5 : 9
+console.log(res)
