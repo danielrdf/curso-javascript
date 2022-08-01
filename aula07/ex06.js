2cd35cfb3ba3a126c2ea03bae0e07960a34450d2
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-01 17:53:02 -0300

    Update ex06.js

--- aula07/ex06.js
+++ aula07/ex06.js
@@ -5,3 +5,14 @@ console.log(5 * 2)
 console.log(5 / 2)
 console.log(5 % 2)
 console.log(5 ** 2)
+
+// ordem de precedência: () -> ** -> (*, /, %) -> (+, -)
+console.log(5 + 3 / 2)
+console.log((5 + 3) / 2)
+
+var x = 5
+console.log(x++)
+console.log(x)
+
+var y = 10
+console.log(++y)
