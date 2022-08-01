2013432031c88072aa651ceafe9499ea90f3294f
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-01 16:58:28 -0300

    atualização aula06

---
+++ aula06/ex02.js
@@ -0,0 +1,3 @@
+var nome = window.prompt('Qual é seu nome?') // var ou let; pode-se utilizar '' ou "" ou `` para strings
+window.alert('Bem-vindo, ' + nome + '!')
+window.alert(`Bem-vindo, ${nome}!`) // crase para template string
