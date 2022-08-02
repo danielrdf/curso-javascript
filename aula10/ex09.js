b8544475435d49859172b28525b21e09092360ec
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-02 11:02:25 -0300

    Update ex09.js

--- aula10/ex09.js
+++ aula10/ex09.js
@@ -1,11 +1,21 @@
+/*
+Alguns eventos DOM com movimento do mouse:
+mouseover; mousemove; mousedown; mouseup; click; mouseout
+
+https://developer.mozilla.org/pt-BR/docs/Web/Events
+*/
+
 var a = window.document.getElementById('area')
-function clicar() {
+
+function clicar() {   
     a.innerText = 'Clicou!'
     a.style.background = '#355764'
 }
+
 function entrar() {
     a.innerText = 'Entrou!'
 }
+
 function sair() {
     a.innerText = 'Saiu!'
     a.style.background = '#5A8F7B'
