9881fd599f52f5998a6a70682317017949ed463c
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-02 17:55:53 -0300

    e01

--- exercícios/e01/script.js
+++ exercícios/e01/script.js
@@ -3,18 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
+    msg.innerHTML = `Agora são ${hora} horas.`
+
     if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+        // Bom dia!
         img.src = 'manha.png'
-        document.body.style.background = '#DDC634'
+        document.body.style.background = '#cfbc44' // #DDC634
     } else if (hora >= 12 && hora < 18) {
-        // BOA TARDE!
+        // Boa tarde!
         img.src = 'tarde.png'
-        document.body.style.background = '#648BB2'
+        document.body.style.background = '#648BB2' // #648BB2
     } else {
-        // BOA NOITE!
+        // Boa noite!
         img.src = 'noite.png'
-        document.body.style.background = '#BD7314'
+        document.body.style.background = '#b17648' // #BD7314
     }
 }
