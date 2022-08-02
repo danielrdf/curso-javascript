8b6f66c338a90a8eed8e4e3e698b1dc55ca76fc5
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-02 10:10:49 -0300

    atualização aula10

---
+++ aula10/ex09.js
@@ -0,0 +1,12 @@
+var a = window.document.getElementById('area')
+function clicar() {
+    a.innerText = 'Clicou!'
+    a.style.background = '#355764'
+}
+function entrar() {
+    a.innerText = 'Entrou!'
+}
+function sair() {
+    a.innerText = 'Saiu!'
+    a.style.background = '#5A8F7B'
+}
