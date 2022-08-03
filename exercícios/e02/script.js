e713996b8343eaf887891ff24757c79e40f3d3f8
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-03 14:39:06 -0300

    atualização e02

--- exercícios/e02/script.js
+++ exercícios/e02/script.js
@@ -11,15 +11,40 @@ function verificar() {
         var idade = ano - Number(fAno.value)
         var genero = ''
         var img = document.createElement('img') // criando imagem na página dinamicamente 
+        img.setAttribute('id', 'foto')
         if (fSex[0].checked) {
             genero = 'Homem'
-            if (idade < 15) {
-                
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'homem-crianca.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'homem-jovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'homem-idoso.png')
             }
         } else if (fSex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'mulher-crianca.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'mulher-jovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'mulher-idosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos <strong>${genero}</strong> com <strong>${idade}</strong> anos.`
+        res.appendChild(img)
     }
 }
