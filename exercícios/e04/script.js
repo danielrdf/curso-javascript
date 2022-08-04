5ef28af52677c9743f85497cf9d815f4643ed000
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-04 14:39:35 -0300

    atualização e04

--- exercícios/e04/script.js
+++ exercícios/e04/script.js
@@ -6,9 +6,12 @@ function tabuada() {
         window.alert('Por favor, digite um número')
     } else {
         let n = Number(num.value)
+        tab.innerHTML = ''
         for (let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
-            `${n} x ${c} = ${n * c}`
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}` // útil para outras linguagens como PHP
+            tab.appendChild(item)
         }
     }
 
