e2b5529f94ee2236a8eae3bb1b90e92e66111ac5
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-04 14:55:46 -0300

    Update ex22.js

--- aula15/ex22.js
+++ aula15/ex22.js
@@ -15,5 +15,8 @@ for (var pos in valores) {
 }
 
 console.log('')
+
 var p = valores.indexOf(7)
 console.log(`O valor 7 está na posição ${p}`)
+var q = valores.indexOf(3)
+console.log(`O valor 3 está na posição ${q}`) // -1 significa que 3 não consta no array
