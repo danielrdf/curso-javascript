5fe52ab0f74d712e4a3bab9d7ddf031d2dd6edfc
Daniel Fonseca <89491258+danielrdf@users.noreply.github.com>
2022-08-04 14:47:42 -0300

    Update ex21.js

--- aula15/ex21.js
+++ aula15/ex21.js
@@ -1,5 +1,16 @@
 let num = [5, 8, 2, 9, 3]
-num.push(1)
-num.sort()
+
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro elemento do vetor é ${num[0]}`)
+console.log('')
+
+num.push(1) // num = [5, 8, 2, 9, 3, 1]
+
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro elemento do vetor é ${num[0]}`)
+console.log('')
+
+num.sort() // num = [1, 2, 3, 5, 8, 9]
+
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro elemento do vetor é ${num[0]}`)
